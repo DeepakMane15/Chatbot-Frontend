748901d0dcb99ee620a8f82d6768714e1e88b269
Deepak Mane <manedeep2001@gmail.com>
2023-01-03 00:30:17 +0530

    added functionalities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IframeComponent } from './iframe/iframe.component';
 import { ChatboxComponent } from './chatbox/chatbox.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/chatbox/chatbox-service.service.ts
+++ src/app/chatbox/chatbox-service.service.ts
@@ -7,7 +7,14 @@ import { Observable, Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class ChatboxServiceService {
-url = 'http://127.0.0.1:5000/predict'
+chatbot_url = 'http://127.0.0.1:5000/predict'
+business_url = 'http://localhost:3000/agent/BusinessData'
+business_vrf = 'http://localhost:3000/agent/BusinessVerification'
+autoResponse_url = 'http://localhost:3000/agent/StaticAutoResponse'
+userConversation_url = 'http://localhost:3000/users/UserConversation'
+sendEmail_url = 'http://localhost:3000/agent/SendEmail'
+endChat_url = 'http://localhost:3000/agent/EndChat'
+
   constructor(private http: HttpClient) { }
 
   private getHeader(): HttpHeaders {
@@ -17,7 +24,28 @@ url = 'http://127.0.0.1:5000/predict'
   headers = headers.append('Content-Type', 'application/json');
   return headers;
 }
+
+GetBusinessData(data:any):Observable<any>{
+  return this.http.post(this.business_url,data);
+}
+VerifyBusiness(data:any):Observable<any>{
+  return this.http.post(this.business_vrf,data);
+}
+GetAutoResponse(data:any):Observable<any>{
+  return this.http.post(this.autoResponse_url,data);
+}
+
 sendMsg(data:any){
-  return this.http.post(this.url,data, { headers: this.getHeader() });
+  return this.http.post(this.chatbot_url,data, { headers: this.getHeader() });
+}
+
+getUserConversation(data:any):Observable<any>{
+  return this.http.post(this.userConversation_url,data, { headers: this.getHeader() });
+}
+sendEmail(data:any):Observable<any>{
+  return this.http.post(this.sendEmail_url,data, { headers: this.getHeader() });
+}
+endChat(data:any):Observable<any>{
+  return this.http.post(this.endChat_url,data, { headers: this.getHeader() });
 }
 }
